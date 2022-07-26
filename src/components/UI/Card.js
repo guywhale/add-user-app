1740170b51505236180b8a0e5ba1b8081138c46f
Guy Whale <guywhale@gmail.com>
2022-07-26 12:10:21 +0100

    Add top margin to Card components

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Card = styled.div`
 	width: 100%;
 	max-width: 700px;
-	margin: auto;
+	margin: 30px auto 0;
 	padding: 20px;
 	border-radius: 12px;
 	box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);

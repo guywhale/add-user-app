6f7bf3a81ba009f90f64131c120dd0a35a33ec21
Guy Whale <guywhale@gmail.com>
2022-07-26 10:33:38 +0100

    Add card component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+	width: 100%;
+	max-width: 700px;
+	margin: auto;
+	padding: 20px;
+	border-radius: 12px;
+	box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+	background-color: #fff;
+`;
+
+export default Card;

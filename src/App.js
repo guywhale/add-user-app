f24bbf6b8523e88ded395e6aa537574697b14bd5
Guy Whale <guywhale@gmail.com>
2022-07-26 12:09:27 +0100

    Add AddUser and Users components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './components/UI/Container';
 import AddUser from './components/AddUser/AddUser';
+import Users from './components/Users/Users';
 
 const App = () => {
+	const [userList, setUserList] = useState([]);
+
+	const addUserHandler = (addedUser) => {
+		const newUser = {
+			...addedUser,
+			key: Math.random().toString(),
+		};
+
+		setUserList((prevUsers) => {
+			return [...prevUsers, newUser];
+		});
+	};
+
 	return (
 		<div className="App">
 			<Container>
-				<AddUser />
+				<AddUser onAddUser={addUserHandler} />
+				<Users users={userList} />
 			</Container>
 		</div>
 	);

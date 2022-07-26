3e5d31d64bb4451815b276b6c76b418091d79245
Guy Whale <guywhale@gmail.com>
2022-07-26 15:22:02 +0100

    Use React Portals to render the error modal components before the root element, as this is better semantically

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from '../Button/Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={classes.modal} style={{ padding: 0 }}>
+			<header className={classes.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
 const ErrorModal = (props) => {
 	return (
 		<>
-			<div className={classes.backdrop} onClick={props.onConfirm}></div>
-			<Card className={classes.modal} style={{ padding: 0 }}>
-				<header className={classes.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={classes.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={props.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById('backdrop-root')
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={props.title}
+					message={props.message}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById('overlay-root')
+			)}
 		</>
 	);
 };
